c95cc7f77dcb882d5073a035646c66389227b52f
doe-on <fovusshi01197@gmail.com>
2026-04-02 10:18:09 +0900

    Update 3-5.c

--- code/3-5/3-5.c
+++ code/3-5/3-5.c
@@ -10,13 +10,14 @@ void my_print(char type, void* x) {
 }
 
 int main() {
-    char c = 'A';
-    int d = 10;
-    char str[] = "hello";
+    int a = 0x61;
+    char b = 'b';
+    char* c = "hello world";
 
-    my_print('C', &c);
-    my_print('D', &d);
-    my_print('S', str);
+    my_print('C', &a);
+    my_print('D', &b);
+    my_print('S', c);
+    my_print('C', c);
 
     return 0;
 }
