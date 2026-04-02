ef140c305edcfea0acd2b0b0e220cdc0dae41582
doe-on <fovusshi01197@gmail.com>
2026-04-02 10:06:17 +0900

    Update 3-4.c

---
+++ code/3-4/3-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void inc(int* x) {
+  if( p != NULL )
+      return;
+  *x = *x + 1;
+  printf("%d\n", *x);
+}
+
+int main () {
+  int x = 3;
+
+  int(&x);
+  int(NULL);
+
+  return 0;
+
+}
+
+  
